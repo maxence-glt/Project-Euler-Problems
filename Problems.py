780f2ccb9ac2a559a404d043f8f68b8287c927ab
maxence-glt <mgilloteaux05@gmail.com>
2023-07-26 16:13:15 -0400

    Problem 3 finished! Problem 2 also finished but the time complexity is absol

--- Problems.py
+++ Problems.py
@@ -1,6 +1,5 @@
-# Problem 1 - Multiples of 3 and 5
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23
-# Find the sum of all the multiples of 3 or 5 below 1000.
+# Problem 1 (5% difficulty) - Multiples of 3 and 5 - https://projecteuler.net/problem=1
+
 
 # Iteratively:
 def sumMultiples(x, y):        #x and y are the multiples
@@ -13,7 +12,7 @@ def sumMultiples(x, y):        #x and y are the multiples
             index += 1
     return total
 
-print(sumMultiples(3, 5))
+#print(sumMultiples(3, 5))
 
 
 # Recursively:
@@ -26,9 +25,67 @@ def sumMultiplesRecursive(x, y, z):        #z is the amount of sum limit - 1
         else:
             return sumMultiplesRecursive(x, y, z - 1)
 
-print(sumMultiplesRecursive(3, 5, 999))        #Reaches recursion limit, but does work with lower z values  
+#print(sumMultiplesRecursive(3, 5, 999))        #Reaches recursion limit, but does work with lower z values  
 
 
 #Using a list comprehension
 multiples = sum(x for x in range(1000) if x % 3 == 0 or x % 5 == 0)
-print(multiples)
+
+#print(multiples)
+
+
+
+
+
+#Problem 2 (5%) - Even Fibonacci Numbers - https://projecteuler.net/problem=2
+#Fibonacci: (start with 1 and 2) 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+# Recursively:
+def fibonacciSeq(x):
+    if x <= 2:
+        return x
+    else:
+        return fibonacciSeq(x - 1) + fibonacciSeq(x - 2)
+
+
+def fibonacciSum(x):
+    index = 1
+    sum = 0
+    number = fibonacciSeq(index)
+    while number < x:
+        if (number % 2 == 0):
+            sum += number
+            index += 1
+            number = fibonacciSeq(index)
+        elif (number % 2 != 0):
+            index += 1
+            number = fibonacciSeq(index)
+        elif number >= x:
+            return sum
+        
+
+print(fibonacciSum(4000000))        # It works at lower levels but starts to take a long time above 50, so
+                                    # technically its a solution just not an optimal one.
+
+# I will come back to this one!
+
+
+
+
+
+#Problem 3 (5%) - Largest Prime Factor - https://projecteuler.net/problem=3
+def primeFactors(x):
+    index = 2
+    primes = []
+    while index <= x:
+        if x % index == 0:
+            primes.append(index)
+            x = x / index
+        else:
+            index += 1
+    return primes       # This returns ALL factors, the maximum should be fairly easy to decipher
+                        # however I could always just add max(primes) at the end there!
+
+
+
+
